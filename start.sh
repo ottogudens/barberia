5f407cdd4ffa18c8a718b8003287744f27349be1
ottogudens <ottogudenschwager@gmail.com>
2026-02-12 17:55:13 -0300

    Fix 502 error: simplify PHP-FPM config and improve startup reliability

--- start.sh
+++ start.sh
@@ -4,11 +4,12 @@
 sed -i "s/\$PORT/$PORT/g" /app/nginx.conf
 
 # Configure PHP-FPM user and group dynamically
-sed -i "s/FPM_USER/$(whoami)/g" /app/php-fpm.conf
-sed -i "s/FPM_GROUP/$(id -gn)/g" /app/php-fpm.conf
-
 # Start PHP-FPM in the background
+# We use php-fpm (default in many nixpacks images)
 php-fpm -y /app/php-fpm.conf -R &
+
+# Wait a moment to ensure it doesn't crash immediately
+sleep 2
 
 # Start Nginx in the foreground
 nginx -c /app/nginx.conf
