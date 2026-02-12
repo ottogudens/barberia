b9cd4c9166a8a8e2990b0f89abf552d037f2cd3a
ottogudens <ottogudenschwager@gmail.com>
2026-02-12 13:21:38 -0300

    Fix 502 error: remove reference to deleted init_railway_db.php in start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,6 @@ sed -i "s/\$PORT/$PORT/g" /app/nginx.conf
 sed -i "s/FPM_USER/$(whoami)/g" /app/php-fpm.conf
 sed -i "s/FPM_GROUP/$(id -gn)/g" /app/php-fpm.conf
 
-# Initialize Database
-echo "Running Database Initialization..."
-php /app/init_railway_db.php
-
 # Start PHP-FPM in the background
 php-fpm -y /app/php-fpm.conf -R &
 
